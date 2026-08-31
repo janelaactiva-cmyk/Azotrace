4a2d2760bca3553da5a4ee1f525391f366edf9ae
janelaactiva-cmyk <janelaactiva@gmail.com>
2026-08-31 12:04:42 +0000

    Fix on home page

--- apps/web/app/home/page.tsx
+++ apps/web/app/home/page.tsx
@@ -1,10 +1,14 @@
+import { connection } from 'next/server'; // 👈 1. Importe o connection
+
 import { PageBody, PageHeader } from '@kit/ui/page';
 
 import { DashboardContent } from '~/app/dashboard/_components/dashboard-content';
 
 
 
-export default function HomePage() {
+export default async function HomePage() {
+await connection(); // 👈 2. Aguarde pela conexão para forçar o comportamento dinâmico
+
   return (
     <>
       <PageHeader description={'Your SaaS at a glance'} />
