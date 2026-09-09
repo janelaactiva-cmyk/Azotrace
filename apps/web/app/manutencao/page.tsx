c9043491d76b84b09edbc8ba2ed980dc40f68523
janelaactiva-cmyk <janelaactiva@gmail.com>
2026-09-09 10:29:53 +0000

    Implementação de site em construção

---
+++ apps/web/app/manutencao/page.tsx
@@ -0,0 +1,19 @@
+export default function EmConstrucaoPage() {
+  return (
+    <main style={{
+      minHeight: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      background: '#f3f4f6',
+      fontFamily: 'sans-serif',
+      padding: '20px',
+      textAlign: 'center'
+    }}>
+     <img src="/assets/images/imagem.png" alt="site em construção" />
+     
+     
+    </main>
+  );
+}
--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -13,7 +13,7 @@ const nextConfig = {
     return [
       {
         source: '/',
-        destination: '/landing',
+        destination: '/manutencao',
         permanent: false,
       },
     ];
